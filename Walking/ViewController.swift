f5a771b88996cb255b467c01066c78738ca4e0f8
YifangZhang <yifangzhang2009@gmail.com>
2016-11-29 20:35:46 -0600

    init

--- Walking/ViewController.swift
+++ Walking/ViewController.swift
@@ -9,6 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /* init all labels */
+    @IBOutlet weak var label_acc_x: UILabel!
+    @IBOutlet weak var label_acc_y: UILabel!
+    @IBOutlet weak var label_acc_z: UILabel!
+    @IBOutlet weak var label_gyro_x: UILabel!
+    @IBOutlet weak var label_gyro_y: UILabel!
+    @IBOutlet weak var label_gyro_z: UILabel!
+    @IBOutlet weak var label_mag_x: UILabel!
+    @IBOutlet weak var label_mag_y: UILabel!
+    @IBOutlet weak var label_mag_z: UILabel!
+    @IBOutlet weak var label_yaw: UILabel!
+    @IBOutlet weak var label_pitch: UILabel!
+    @IBOutlet weak var label_roll: UILabel!
+    /* end of label init */
+    
+    /* init all sensors */
+    var acc_x : [Double] = []
+    var acc_z : [Double] = []
+    var acc_y : [Double] = []
+    var time_acc : [String] = []
+    
+    var gyro_x : [Double] = []
+    var gyro_z : [Double] = []
+    var gyro_y : [Double] = []
+    var time_gyro : [String] = []
+    
+    var mag_x : [Double] = []
+    var mag_z : [Double] = []
+    var mag_y : [Double] = []
+    var time_mag : [String] = []
+    
+    var yaw_ : [Double] = []
+    var pitch_ : [Double] = []
+    var roll_ : [Double] = []
+    var time_yaw : [String] = []
+
+    var time_compass : [String] = []
+    var compass : [Double] = []
+    /* end of sensor init */
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
